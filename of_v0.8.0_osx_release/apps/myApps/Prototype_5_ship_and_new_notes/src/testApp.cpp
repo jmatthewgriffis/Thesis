a7df8800a9640a06058f0d2c4fb955b093bdcf6d
J. Matthew Griffis <grifj153@newschool.edu>
2014-02-04 13:32:52 -0500

    Disabled jumping to the boss battle for the time being.

--- of_v0.8.0_osx_release/apps/myApps/Prototype_5_ship_and_new_notes/src/testApp.cpp
+++ of_v0.8.0_osx_release/apps/myApps/Prototype_5_ship_and_new_notes/src/testApp.cpp
@@ -1358,14 +1358,14 @@ void testApp::keyPressed(int key){
                 gameState = 1;
             }
             // Go to boss battle if player has reached end of tutorial.
-            else if ( gameState == 1 && myPlayer.pos.x > obstacleList[ obstacleList.size() - 1 ].pos.x + obstacleList[ obstacleList.size() - 1 ].wide + 400 ) {
+            /*else if ( gameState == 1 && myPlayer.pos.x > obstacleList[ obstacleList.size() - 1 ].pos.x + obstacleList[ obstacleList.size() - 1 ].wide + 400 ) {
                 //else if ( gameState == 1 ) {
                 cleanup();
                 gameState = 2;
                 myPlayer.setup();
                 bHighlightNote = true;
                 getThisOne = 0;
-            }
+            }*/
             break;
             
         case OF_KEY_SHIFT:
