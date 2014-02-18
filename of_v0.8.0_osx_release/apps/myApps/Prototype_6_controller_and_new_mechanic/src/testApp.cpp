17dfe2d3404bc229636ca566b053b8ee9510c668
J. Matthew Griffis <grifj153@newschool.edu>
2014-02-18 10:59:28 -0500

    Finished converting the tutorial to dynamic sizing.

--- of_v0.8.0_osx_release/apps/myApps/Prototype_6_controller_and_new_mechanic/src/testApp.cpp
+++ of_v0.8.0_osx_release/apps/myApps/Prototype_6_controller_and_new_mechanic/src/testApp.cpp
@@ -1274,65 +1274,65 @@ void testApp::fSetupTutorial() {
     return;
     
     // Jump!
-    float x = 1000;
+    float x = iScaler * 40;
     Obstacle tmp;
-    tmp.setup( ofVec2f( x, ofGetHeight() ), 100, 75, true );
+    tmp.setup( ofVec2f( x, ofGetHeight() ), iScaler * 4, iScaler * 3, true );
     obstacleList.push_back( tmp );
     
     // Jump off a note to overcome an obstacle.
-    float x2 = 1800;
+    float x2 = iScaler * 72;
     addObject( "d3", x2, -1 );
     Obstacle tmp2;
-    tmp2.setup( ofVec2f( x2 + 200, ofGetHeight() ), 100, 150, true );
+    tmp2.setup( ofVec2f( x2 + iScaler * 8, ofGetHeight() ), iScaler * 4, iScaler * 6, true );
     obstacleList.push_back( tmp2 );
     
     // Record and replay a note to overcome an obstacle.
-    float x3 = 2300;
+    float x3 = iScaler * 92;
     addObject( "d3", x3, -1 );
-    addObject( "a3", x3 + 745, -1 );
+    addObject( "a3", x3 + iScaler * 29.8, -1 );
     Obstacle tmp3;
-    tmp3.setup( ofVec2f( x3 + 2000, ofGetHeight() ), 100, 150, true );
+    tmp3.setup( ofVec2f( x3 + iScaler * 80, ofGetHeight() ), iScaler * 4, iScaler * 6, true );
     obstacleList.push_back( tmp3 );
     
     // Spring off notes to get to higher notes and overcome a tall obstacle.
-    float x4 = 4600;
+    float x4 = iScaler * 184;
     addObject( "d3", x4, -1 );
-    //    addObject( "g4", x4 + 200, -1 );
-    addObject( "c4", x4 + 400, -1 );
-    addObject( "g3", x4 + 700, -1 );
+    //    addObject( "g4", x4 + iScaler * 8, -1 );
+    addObject( "c4", x4 + iScaler * 16, -1 );
+    addObject( "g3", x4 + iScaler * 28, -1 );
     
-    float x5 = x4 + 1100;
+    float x5 = x4 + iScaler * 44;
     addObject( "g3", x5, -1 );
-    addObject( "d3", x5 + 100, -1 );
-    //    addObject( "c5", x5 + 200, -1 );
+    addObject( "d3", x5 + iScaler * 4, -1 );
+    //    addObject( "c5", x5 + iScaler * 8, -1 );
     Obstacle tmp4;
-    tmp4.setup( ofVec2f( x5 + 400, ofGetHeight() ), 100, 300, true );
+    tmp4.setup( ofVec2f( x5 + iScaler * 16, ofGetHeight() ), iScaler * 4, iScaler * 12, true );
     obstacleList.push_back( tmp4 );
     
     // Record multiple notes before replaying to overcome a tall obstacle before the notes expire.
-    addObject( "b3", x5 + 600, -1 );
-    addObject( "e3", x5 + 700, -1 );
+    addObject( "b3", x5 + iScaler * 24, -1 );
+    addObject( "e3", x5 + iScaler * 28, -1 );
     
-    float x6 = 7500;
+    float x6 = iScaler * 300;
     addObject( "b3", x6, -1 );
-    addObject( "e3", x6 + 200, -1 );
+    addObject( "e3", x6 + iScaler * 8, -1 );
     Obstacle tmp5;
-    tmp5.setup( ofVec2f( x6 + 1450, ofGetHeight() ), 100, 300, true );
+    tmp5.setup( ofVec2f( x6 + iScaler * 58, ofGetHeight() ), iScaler * 4, iScaler * 12, true );
     obstacleList.push_back( tmp5 );
     
     // Ride a moving note to overcome a tall obstacle.
     Obstacle tmp6;
-    tmp6.setup( ofVec2f( x6 + 1550, ofGetHeight() ), 100, 200, true );
+    tmp6.setup( ofVec2f( x6 + iScaler * 62, ofGetHeight() ), iScaler * 4, iScaler * 8, true );
     obstacleList.push_back( tmp6 );
     Obstacle tmp7;
-    tmp7.setup( ofVec2f( x6 + 1650, ofGetHeight() ), 100, 100, true );
+    tmp7.setup( ofVec2f( x6 + iScaler * 66, ofGetHeight() ), iScaler * 4, iScaler * 4, true );
     obstacleList.push_back( tmp7 );
     
-    float x7 = 9250;
+    float x7 = iScaler * 370;
     addObject( "b3", x7, -1 );
-    objectList[ objectList.size() - 1 ].vel.set( -3.0, 0.0 );
+    objectList[ objectList.size() - 1 ].vel.set( float( -( iScaler / 8.3333 ) ), 0.0 );
     Obstacle tmp8;
-    tmp8.setup( ofVec2f( x7 + 750, ofGetHeight() ), 100, 300, true );
+    tmp8.setup( ofVec2f( x7 + iScaler * 30, ofGetHeight() ), iScaler * 4, iScaler * 12, true );
     obstacleList.push_back( tmp8 );
 }
 
